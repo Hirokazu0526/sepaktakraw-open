0896962dfca602623d2182df49bbadc17d6d4ab0
h6k2 <sepak.hirokazu10@gmail.com>
2020-08-11 15:55:35 +0900

    ローディング、テキストアニメーション追加

--- scripts/libs/text-animation.js
+++ scripts/libs/text-animation.js
@@ -10,3 +10,53 @@ document.addEventListener('scroll', function(){
 
 })
 
+const targetNav = document.querySelector('#header-top');
+document.addEventListener('scroll', function(){
+    const targetNavDistance = targetNav.getBoundingClientRect().bottom
+
+    if(targetNavDistance <= 0){  
+        document.getElementById('groval-nav').classList.add('nav_fixed');
+    }
+    if(targetNavDistance > 0){
+        document.getElementById('groval-nav').classList.remove('nav_fixed');
+    }
+})
+
+const targetImg = document.querySelector('#main-slider');
+document.addEventListener('scroll', function(){
+    const targetImgDistance = targetNav.getBoundingClientRect().top + targetImg.clientHeight * .3
+
+    if(targetImgDistance <= 0){  
+        document.getElementById('bg').classList.add('bg-active');
+    }
+})
+
+
+
+window.addEventListener('load',function(){
+    const animationTargetElements = document.querySelectorAll('.textAnimation');
+    for (let i = 0; i < animationTargetElements.length; i++) {
+        const taElement = animationTargetElements[i];
+            texts = taElement.textContent,
+            textsArray = [];
+           
+        taElement.textContent = '';
+    
+        for (let j = 0; j < texts.split("").length; j++) {
+            const t = texts.split("")[j]
+            if (t === " ") {
+              textsArray.push(" ");  
+            }else{
+            textsArray.push('<span><span style="animation-delay: ' + (.7 +(j * .07)) + 's;">' + t + '</span></span>')  
+            } 
+        }
+        
+        for (let k = 0; k < textsArray.length; k++) {
+            taElement.innerHTML += textsArray[k]
+        }
+            
+    }
+   
+})
+
+
